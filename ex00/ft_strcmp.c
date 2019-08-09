d074805672ac34d9083d7760375ede895a97f547
Milos Andric <mandric@z3r7p2.le-101.fr>
2019-08-09 16:35:57 +0200

    mom

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: mandric <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/08/06 21:04:08 by mandric      #+#   ##    ##    #+#       */
-/*   Updated: 2019/08/08 16:30:01 by mandric     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/08/09 16:35:39 by mandric     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,12 +18,12 @@ int	ft_strcmp(char *s1, char *s2)
 	unsigned int res_i;
 	unsigned int res_p;
 
-	i = -1;
+	i = 0;
 	p = -1;
 	res_i = 0;
 	res_p = 0;
-	while (s1[++i] != '\0')
-		res_i += s1[i];
+	while (s1[i++] == s2[i])
+		;
 	while (s2[++p] != '\0')
 		res_p += s2[i];
 	return (res_i - res_p);
